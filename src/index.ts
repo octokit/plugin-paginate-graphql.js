5596fa6bf1ca861315517fd01e8d96fb8553a984
wolfy1339 <webmaster@wolfy1339.com>
2024-04-30 14:11:44 -0400

    docs: add comment to export

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { createPaginate } from "./paginate.js";
 export type { PageInfoForward, PageInfoBackward } from "./page-info.js";
 export { VERSION } from "./version.js";
 
+// Export the paginateGraphQLInterface type in order to fix TypeScript errors in downstream projects
+// The inferred type of 'Octokit' cannot be named without a reference to '@octokit/core/node_modules/@octokit/graphql/types'. This is likely not portable. A type annotation is necessary.
 export type paginateGraphQLInterface = {
   graphql: Octokit["graphql"] & {
     paginate: ReturnType<typeof createPaginate> & {
